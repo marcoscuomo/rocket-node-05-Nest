030b485c626295aa8e6b0e018607983051915c30
Marcos Souza <marcos.souza@beeleads.com.br>
2025-02-17 15:59:45 -0300

    Criando serviço do Prisma

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
+import { PrismaClient } from '@prisma/client'
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({
+      // query pode ser passado para visualizar as queries
+      log: ['warn', 'error'],
+    })
+  }
+
+  onModuleInit() {
+    return this.$connect()
+  }
+
+  onModuleDestroy() {
+    return this.$disconnect()
+  }
+}
