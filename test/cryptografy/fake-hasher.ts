fc2efeb487f2edecf80037ea4b399420db5b5934
Marcos Souza <marcos.souza@beeleads.com.br>
2025-02-24 17:21:08 -0300

    feat: implementa estratégia de rotas privadas por padrão

--- test/cryptografy/fake-hasher.ts
+++ test/cryptografy/fake-hasher.ts
@@ -1,7 +1,7 @@
-import { HasheComparer } from '@/domain/forum/application/cryptography/hash-comparer'
+import { HashComparer } from '@/domain/forum/application/cryptography/hash-comparer'
 import { HasheGenerator } from '@/domain/forum/application/cryptography/hasher-generator'
 
-export class FakeHasher implements HasheGenerator, HasheComparer {
+export class FakeHasher implements HasheGenerator, HashComparer {
   async hash(plain: string): Promise<string> {
     return plain.concat('-hashed')
   }
