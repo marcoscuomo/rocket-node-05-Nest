e3c82481b02b78a65b5596698a565b670c097009
Marcos Souza <marcos.souza@beeleads.com.br>
2025-03-03 00:23:56 -0300

    Criando Mappers do Prisma

---
+++ src/infra/database/prisma/mappers/prisma-answer-attachment-mapper.ts
@@ -0,0 +1,19 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Attachment as PrismaAttachment } from '@prisma/client'
+import { AnswerAttachment } from '@/domain/forum/enterprise/entities/answer-attachment'
+
+export class PrismaAnswerAttachmenttMapper {
+  static toDomain(raw: PrismaAttachment): AnswerAttachment {
+    if (!raw.answerId) {
+      throw new Error('Invalid attachment type.')
+    }
+
+    return AnswerAttachment.create(
+      {
+        attachmentId: new UniqueEntityID(raw.id),
+        answerId: new UniqueEntityID(raw.answerId),
+      },
+      new UniqueEntityID(raw.id),
+    )
+  }
+}
